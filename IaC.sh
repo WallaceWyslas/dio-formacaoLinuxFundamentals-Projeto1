ed625b0fcbec34d0877acfd9294322f1383b6901
WallaceWyslas <wallacewyslas@gmail.com>
2024-04-16 15:48:12 -0300

    Com comentários

--- IaC.sh
+++ IaC.sh
@@ -5,8 +5,7 @@
 cd /
 
 # Variáveis - EDITAR
-groups=("GRP_ADM" "GRP_VEN" "GRP_SEC") #Grupos
-directors770=("adm" "ven" "sec") #Diretórios - Acesso do Root e Grupos
+directors770=("ADM" "VEN" "SEC") #Diretórios - Acesso do Root e Grupos
 directors777=("publico") #Diretórios - Acesso total a todos os diretórios
 usersAdm=("carlos" "maria" "joao") #Usuários ADM
 usersVen=("debora" "sebastiana" "roberto") #Usuários VEN
@@ -16,10 +15,10 @@ usersSec=("josefina" "amanda" "rogerio") #Usuários SEC
 echo " "
 echo "Criando grupos..."
 
-for makeGroup in "${groups[@]}"
+for makeGroup in "${directors770[@]}"
 do
-    groupadd "$makeGroup"
-    echo "Grupo '$makeGroup' criado com sucesso!"
+    groupadd "GRP_$makeGroup"
+    echo "Grupo 'GRP_$makeGroup' criado com sucesso!"
 done
 
 # Criação de diretorios
@@ -34,20 +33,39 @@ do
         echo "Diretório '$makeDirector' criado e adicionado ao grupo root!"
 done
 
-mkdir adm
-chown -R :GRP_ADM /adm
-chmod 770 adm
-echo "Diretório adm criado e adicionado ao grupo GRP_ADM!"
+# for makeDirector in "${directors770[@]}"
+# do
+#     Director="$makeDirector"
+#     Grupo="${groups[$makeDirector]}"
 
-mkdir ven
-chown -R :GRP_VEN /ven
-chmod 770 ven
-echo "Diretório ven criado e adicionado ao grupo GRP_VEN!"
+#     mkdir "$Director"
+#     chmod 770 "$Director"
+#     chown -R ":$Grupo" /"$Director"
+#     echo "Diretório '$Director' criado e adicionado ao grupo '$Grupo'!"
+# done
 
-mkdir sec
-chown -R :GRP_SEC /sec
-chmod 770 sec
-echo "Diretório sec criado e adicionado ao grupo GRP_SEC!"
+for makeDirector in "${directors770[@]}"
+do
+    mkdir "$makeDirector"
+    chmod 770 "$makeDirector"
+    chown -R ":GRP_$makeDirector" "/$makeDirector"
+    echo "Diretório '$makeDirector' criado e adicionado ao grupo 'GRP_$makeDirector'!"
+done
+
+# mkdir adm
+# chown -R :GRP_ADM /adm
+# chmod 770 adm
+# echo "Diretório adm criado e adicionado ao grupo GRP_ADM!"
+
+# mkdir ven
+# chown -R :GRP_VEN /ven
+# chmod 770 ven
+# echo "Diretório ven criado e adicionado ao grupo GRP_VEN!"
+
+# mkdir sec
+# chown -R :GRP_SEC /sec
+# chmod 770 sec
+# echo "Diretório sec criado e adicionado ao grupo GRP_SEC!"
 
 # Criação dos usuarios
 echo " "
@@ -55,17 +73,19 @@ echo "Criando usuários ADM..."
 
 for users in "${usersAdm[@]}" #ADM
 do
-    group="${groups[0]}"
+    group="GRP_${directors770[0]}"
 
-    useradd "$users" -s /bin/bash -G "${group}"
+    useradd "$users" -s /bin/bash -G "$group"
     echo "$users:Senha123" | chpasswd
     echo "Usuário '$users' criado com sucesso e adicionado ao '$group'!"
     passwd "$users" -e
 done
 
+echo " "
+echo "Criando usuários VEN..."
 for users in "${usersVen[@]}" #VEN
 do
-    group="${groups[1]}"
+    group="GRP_${directors770[1]}"
 
     useradd "$users" -s /bin/bash -G "${group}"
     echo "$users:Senha123" | chpasswd
@@ -73,9 +93,11 @@ do
     passwd "$users" -e
 done
 
+echo " "
+echo "Criando usuários SEC..."
 for users in "${usersSec[@]}" #SEC
 do
-    group="${groups[2]}"
+    group="GRP_${directors770[2]}"
 
     useradd "$users" -s /bin/bash -G "${group}"
     echo "$users:Senha123" | chpasswd
